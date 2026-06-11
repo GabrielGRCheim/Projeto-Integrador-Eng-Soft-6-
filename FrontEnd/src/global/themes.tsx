80501f94454f20040a780426a28de8eeecd577ab
Josias Junior Santos <josiajrsantos@gmail.com>
2026-06-10 23:14:39 -0300

    feat: Adicionando React Native Expo, Tela de Login ja criada

---
+++ FrontEnd/src/global/themes.tsx
@@ -0,0 +1,9 @@
+export const themas = {
+    colors: {
+        primary: '#4940ff',
+        secondary: '#ffffff',
+        lightGray:'#d7d8d7',
+        gray: 'gray',
+        bdScreen: '#f1f7fa'
+    }
+}
